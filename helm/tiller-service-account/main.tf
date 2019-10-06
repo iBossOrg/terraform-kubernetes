66de5cde82793fd91408a3032c621e243ce3f58a
Petr Řehoř <rx@rx.cz>
2019-10-06 17:29:39 +0200

    Remove dependencies check from tiller-service-account module
    Signed-off-by: Petr Řehoř <rx@rx.cz>

--- helm/tiller-service-account/main.tf
+++ helm/tiller-service-account/main.tf
@@ -1,24 +1,24 @@
 resource "kubernetes_service_account" "tiller" {
   metadata {
-    name      = local.name
-    namespace = local.namespace
+    name      = var.name
+    namespace = var.namespace
   }
 }
 
 resource "kubernetes_cluster_role_binding" "tiller" {
   metadata {
-    name = local.name
+    name = var.name
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = local.cluster_role
+    name      = var.cluster_role
   }
 
   subject {
     kind      = "ServiceAccount"
-    name      = local.name
-    namespace = local.namespace
+    name      = var.name
+    namespace = var.namespace
   }
 }
